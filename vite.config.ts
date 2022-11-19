4d8a9011e8504238eddde5663a62f5be2e7f90a9
Weston Siegenthaler <westons+github@gmail.com>
2022-11-18 22:41:39 -0800

    bundle size analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import analyze from 'rollup-plugin-analyzer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
   build: { minify: 'terser' },
+  plugins: [
+    react(),
+    analyze({ limit: 20, summaryOnly: true })
+  ]
 })
