90d4882a6516dec0369b087c7156d91756f5735e
Weston Siegenthaler <westons+github@gmail.com>
2022-11-19 19:57:49 -0800

    favicon generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import faviconsPlugin from '@darkobits/vite-plugin-favicons'
 import analyze from 'rollup-plugin-analyzer'
 
 // https://vitejs.dev/config/
@@ -7,6 +8,14 @@ export default defineConfig({
   build: { minify: 'terser' },
   plugins: [
     react(),
+    faviconsPlugin({
+      xpath: './src/assets/favicon.svg',
+      icons: {
+        favicons: {
+          source: './src/assets/favicon.svg'
+        }
+      }
+    }),
     analyze({ limit: 20, summaryOnly: true })
   ]
 })
