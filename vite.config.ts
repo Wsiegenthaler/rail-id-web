6e16f5c0ef976ae2a2ca06a6f06feee346656da5
Weston Siegenthaler <westons+github@gmail.com>
2022-11-19 20:27:30 -0800

    trying out jetbrains mono, using vite font plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 import faviconsPlugin from '@darkobits/vite-plugin-favicons'
 import analyze from 'rollup-plugin-analyzer'
 
@@ -14,6 +15,16 @@ export default defineConfig({
         favicons: {
           source: './src/assets/favicon.svg'
         }
+      }
+    }),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            name: 'JetBrains Mono',
+            styles: 'wght@100;200;400;500;600;700;800;900'
+          },
+        ]
       }
     }),
     analyze({ limit: 20, summaryOnly: true })
