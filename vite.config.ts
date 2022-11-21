121d55841beda3196c396cab386463327c2fbc3d
Weston Siegenthaler <westons+github@gmail.com>
2022-11-21 14:48:14 -0800

    css postprocessing

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import faviconsPlugin from '@darkobits/vite-plugin-favicons'
+import lightningcss from 'vite-plugin-lightningcss'
 import analyze from 'rollup-plugin-analyzer'
+
+
+const browserlist = 'last 3 versions, >= 95% in US'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { minify: 'terser' },
   plugins: [
     react(),
+    lightningcss({ browserlist }),
     faviconsPlugin({
       xpath: './src/assets/favicon.svg',
       icons: {
