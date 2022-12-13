262613789f6fa24e0474afa0931cf6d5ea80a802
Weston Siegenthaler <westons+github@gmail.com>
2022-12-13 08:11:39 -0800

    fixing issue where inlined cache-busted svg was occasionally failing to render

--- src/components/util/UncachedSvg.tsx
+++ src/components/util/UncachedSvg.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react"
-import svgToMiniDataUri from 'mini-svg-data-uri'
 import { nanoid } from "nanoid"
+
+import { Buffer } from 'buffer'
 
 type Props = { src: string, className?: string }
 
-// Component which prevents caching of svgs by the browser which sometimes
+const encode64 = (str: string) => Buffer.from(str, 'binary').toString('base64')
+
+// Component which prevents browser caching of svgs which sometimes
 // results in failure to execute any embedded animations.
 function UncachedSvg({ src, className }: Props) {
   const [uid, setUid] = useState<string>(nanoid())
 
   const cacheBust = `<!--${uid}-->`
-  const uri = svgToMiniDataUri(cacheBust + src)
+  const uri = `data:image/svg+xml;base64,${encode64((cacheBust + src))}`
 
   return (<img src={uri} className={className} />)
 }
