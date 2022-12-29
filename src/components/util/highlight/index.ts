0865b164ce1d58509e4d6d3ad4b361c7799c74ba
Weston Siegenthaler <westons+github@gmail.com>
2022-12-29 00:59:33 -0800

    minor

--- src/components/util/highlight/index.ts
+++ src/components/util/highlight/index.ts
@@ -1,6 +1,9 @@
 
+// Highlight state including relevant marker and source positions to be highlighted
 export type HighlightState = 'clear' | { markerId: string, source: number[]  }
 
+// Setter for updating highlight state
 export type SetHighlights = React.Dispatch<React.SetStateAction<HighlightState>>
 
+// Globally provided highlight context including highlight state and setter
 export type HighlightContextType = { state: HighlightState, set: SetHighlights }
